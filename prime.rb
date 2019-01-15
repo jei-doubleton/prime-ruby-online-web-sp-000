dee12fe0cb49e4cc187b7e4daec89dfd3a49a627
J <jennifer.singleton@gmail.com>
2019-01-15 05:09:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(integer)
   array = (2..integer-1).to_a
-  array.none {|number| integer % number == 0}
+  array.none? {|number| integer % number == 0}
   # integer divided by (2..integer-1)
 end
